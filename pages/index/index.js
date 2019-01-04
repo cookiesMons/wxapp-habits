537f0d2faab4a77e2f2b83166337302f2be60c71
linying2019 <30858278+cookiesMons@users.noreply.github.com>
2019-01-04 23:27:59 +0800

    190104打卡页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,67 +4,55 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),  
-    array: [{
-      iconPath:"url",
-      title:"早起",
-      days:"37",
-      completed:true
-    }, {
-        iconPath: "url",
+    array: [
+      {
+        iconPath:"/images/getup.png",
+        title:"早起",
+        days:"37",
+        completed:true
+      }, 
+      {
+        iconPath: "/images/breakfast.png",
         title: "吃早餐",
         days: "23",
         completed: true
-    }, {
-      iconPath: "url",
-      title: "每天读书一小时",
-      days: "16",
-      completed: false
-    }]
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+      }, 
+      {
+        iconPath: "/images/read.png",
+        title: "每天读书一小时",
+        days: "16",
+        completed: false
+      },
+      {
+        iconPath: "/images/leg.png",
+        title: "不翘二郎腿",
+        days: "16",
+        completed: false
+      },
+      {
+        iconPath: "/images/dairy.png",
+        title: "写日记",
+        days: "16",
+        completed: true
+      },
+      {
+        iconPath: "/images/camera.png",
+        title: "拍照",
+        days: "16",
+        completed: false
+      },
+      {
+        iconPath: "/images/sport.png",
+        title: "运动",
+        days: "16",
+        completed: true
+      },
+      {
+        iconPath: "/images/abc.png",
+        title: "背单词",
+        days: "16",
+        completed: true
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
